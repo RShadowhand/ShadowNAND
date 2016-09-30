e6c1ad83927a98e8f796b34c32ee5cc8669364e5
gnmmarechal <gnmpolicemata91@gmail.com>
2016-09-30 17:15:38 +0100

    Updated, with multiple paths, and changes to make it behave like both AW's A9LH and ShadowNAND.

--- payload_stage2/source/main.c
+++ payload_stage2/source/main.c
@@ -2,6 +2,23 @@
 *   main.c
 */
 
+
+/*
+ * DNANwodahS by gnmmarechal
+ * 
+ * Fork of ShadowNAND
+ * 
+ * Licensed under the GPLv2
+ * 
+ * You can use ShadowNAND Installer to install DNANwodahS
+ * 
+ * Base ShadowNAND Version: v0.85
+ * 
+ * DNANwodahS Version: v0.2
+ * 
+ */
+ 
+ 
 #include "types.h"
 #include "buttons.h"
 #include "memory.h"
@@ -48,7 +65,22 @@ void main(void)
         i2cWriteRegister(3, 0x22, 0x2A); //Turn on backlight
         f_unlink("homebrew/3ds/a9nc.bin");
     }
-    else if(fileRead((void *)PAYLOAD_ADDRESS, "homebrew/3ds/boot.bin"))
+    else if(fileRead((void *)PAYLOAD_ADDRESS, "arm9loaderhax.bin")) // Boots from /arm9loaderhax.bin, by default screeninit is off, can be turned on with a keypress at boot
+    {
+        payloadFound = 1;
+        if (HID_PAD != BUTTON_LEFT) // If DPAD_LEFT is not held
+        {
+            ownArm11(0); // Do not init the screen
+        }
+        else // If DPAD_LEFT is held
+        {
+
+            ownArm11(1); // Init the screen
+            clearScreens();
+            i2cWriteRegister(3, 0x22, 0x2A);            
+        }
+    }
+    else if(fileRead((void *)PAYLOAD_ADDRESS, "arm9loaderhax_si.bin")) // Boots from /arm9loaderhax_si.bi, screeninit is on by default.
     {
         payloadFound = 1;
         if (HID_PAD != BUTTON_LEFT) // If DPAD_LEFT is not held
@@ -61,7 +93,35 @@ void main(void)
         {
             ownArm11(0); // Do not init the screen
         }
-    }
+    }    
+    else if(fileRead((void *)PAYLOAD_ADDRESS, "homebrew/3ds/boot.bin")) // Boots from ShadowNAND's standard boot.bin, screeninit is off by default.
+    {
+		payloadFound = 1;
+		if (HID_PAD != BUTTON_LEFT)
+		{
+			ownArm11(0);
+		}
+		else
+		{
+			ownArm11(1);
+			clearScreens();
+			i2cWriteRegister(3, 0x22, 0x2A);			
+		}
+	}    
+    else if(fileRead((void *)PAYLOAD_ADDRESS, "homebrew/3ds/boot_si.bin"))// Boots from /homebrew/3ds/boot_si.bin, screeninit is on by default. 
+    {
+		payloadFound = 1;
+		if (HID_PAD != BUTTON_LEFT)
+		{
+			ownArm11(1);
+			clearScreens();
+			i2cWriteRegister(3, 0x22, 0x2A);
+		}
+		else
+		{
+			ownArm11(0);
+		}
+	}
     else //No payload found/no SD inserted
     {
         payloadFound = 0;
